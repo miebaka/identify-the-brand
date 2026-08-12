5caf80c9c6c178a4090d072e3f362d56fb5664f2
Miebaka Joshua <36106107+miebaka@users.noreply.github.com>
2026-08-12 11:42:02 +0100

    Await Netlify function cold-start initialisation

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,6 +4,13 @@
 import serverless from 'serverless-http';
 import app from '../../server.js';
 
-export const handler = serverless(app, {
+const proxy = serverless(app, {
   requestId: 'x-nf-request-id',
 });
+
+export async function handler(event, context) {
+  // server.js initialises the logo registry and persistence asynchronously.
+  // Do not allow a cold-start request to race that initialisation.
+  await app.locals.ready;
+  return proxy(event, context);
+}
